9ac386f4145efe3a268a8d580198db122fff8095
David Park <davepark97@berkeley.edu>
2022-07-07 21:23:15 -0700

    Define function for computer selection

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(;
+function computerPlay() {
+  let list = ["rock","paper","scissors"];
+  let cpuDecision = list[Math.floor(Math.random() * list.length)]; 
+  return cpuDecision;
+}
+
+console.log(computerPlay());
