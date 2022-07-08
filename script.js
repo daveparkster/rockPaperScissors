c90d6c4f9d0f264125a0990af4951a5e188b5a07
David Park <davepark97@berkeley.edu>
2022-07-08 13:09:23 -0700

    Define a function that plays a round

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
-function computerPlay() {
+let computerPlay = function () {
   let list = ["rock","paper","scissors"];
   let cpuDecision = list[Math.floor(Math.random() * list.length)]; 
   return cpuDecision;
 }
 
-console.log(computerPlay());
+function playRound(playerSelection,computerSelection) {
+  switch (playerSelection) {
+    case 'rock':
+      if(computerSelection === 'paper') {
+        return 'You lose';
+      } else if (computerSelection === 'scissors') {
+        return 'You win';
+      } else {
+        return 'Tie';
+      }
+      break; 
+    case 'scissors':
+      if(computerSelection === 'rock') {
+        return 'You lose';
+      } else if(computerSelection === 'paper') {
+        return 'You win'; 
+      } else {
+        return 'Tie';
+      }
+      break;
+    case 'paper':
+      if(computerSelection === 'scissors') {
+        return 'You lose';
+      } else if(computerSelection === 'rock') {
+        return 'You win';
+      } else {
+        return 'Tie';
+      }
+      break; 
+  }
+}
+
+  console.log(playRound('paper','paper'));
+
+
+
+
+
+
+
+
